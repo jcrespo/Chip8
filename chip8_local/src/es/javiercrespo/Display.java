097819d28bcd9550851b49709423669885aa86f4
Javi <Javi@asus>
2018-02-28 22:20:43 +0100

    Fix the display pain function

--- chip8_local/src/es/javiercrespo/Display.java
+++ chip8_local/src/es/javiercrespo/Display.java
@@ -15,20 +15,13 @@ public class Display extends JFrame {
     
     public Display() {
 	
-	setSize(650,360);
-    //setResizable(false);
+	setSize(660,365);
+    setResizable(false);
     setTitle("Chip8 Display");
 	getContentPane().setBackground(Color.BLACK);
-	//getContentPane().setSize(640, 320);
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	setVisible(true);
 	
-	//Píxeles límite
-	//screen[0][0] = true;
-	//screen[0][63] = true;
-	//screen[31][0] = true;
-	//screen[31][63] = true;
-
 	}
     
     public void paint(Graphics g, short[] screen) {
@@ -37,17 +30,12 @@ public class Display extends JFrame {
 	    	g.setColor(Color.WHITE);
 	    	
 	    	int i=0;
-	    	for (int y=0; y<COL; y++) {   
-	 		   for (int x=0; x<FIL; x++) {
-	 		   //y+4  x+32
-	 		   if ((screen[i] & 0xFF)  == 1) g.fillRect((y*SCALE)+4, (x*SCALE)+32, SCALE, SCALE);
+	    	for (int y=0; y<FIL; y++) {   
+	 		   for (int x=0; x<COL; x++) {
+	 		   if ((screen[i] & 0xFF)  == 1) g.fillRect((x*SCALE)+10, (y*SCALE)+34, SCALE, SCALE);
 	 		   i++;
 	 	   }
-	 		   
 	    }    	
-    	
     }
-    
-    
 
 }
